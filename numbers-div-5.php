3ef5c44d9d5f56925ca165499915d36461abfc90
sergey <tsn3ps@gmail.com>
2020-05-21 01:46:58 +0300

    first commit

---
+++ numbers-div-5.php
@@ -0,0 +1,15 @@
+﻿<?php
+$y=5;
+$sum=0;
+for ($i=20;$i<=45;$i++)
+{
+    if (fmod($i,$y)==0)
+    {
+        echo "Numbers divisible by 5 is:". $i."<br/>";
+    $sum+=$i;
+    }
+}
+echo "Their sum is equal". $sum;
+?>
+
+
